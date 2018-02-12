45557325115ed415d50469b5df84c69287b382ba
mhortillano <mhortillano@github.com>
2018-02-12 10:14:41 -1000

    created array and added the string "I like to eat " before every name

--- SwiftPlaygroundThree.playground/Contents.swift
+++ SwiftPlaygroundThree.playground/Contents.swift
@@ -3,3 +3,29 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//Declare Variable
+
+var fruitbasket: [String]
+
+//Simple String Variable
+
+var simpleString: String
+
+//Initialize an array using an Array Literal
+
+fruitbasket = ["Kiwi", "Mango", "Strawberry", "Banana"]
+
+//print array
+
+print(fruitbasket)
+
+//for-in loop
+
+for name in fruitbasket{
+    //print "I like to eat" before every name in var fruitBasket
+    print("I like to eat " + name + ".")
+}
+
+print(fruitbasket[1])
+print(fruitbasket[0])
